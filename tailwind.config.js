cb78686b8ade2c225dc78c054cf2da0e65db3a85
qutibaalqasim <qalqasim9@gmail.com>
2025-02-27 18:49:26 +0300

    moving out index.html out of the src file and change the directory of paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{html,js}", // Update to your project's file paths
+    "./index.html",
     "node_modules/flowbite/**/*.js", // Include Flowbite
   ],
   theme: {
